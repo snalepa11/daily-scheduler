2c2889752f53adf14e105cb22312b7e05d55e161
Snalepa <snalepa1998@gmail.com>
2024-01-24 22:16:15 -0600

    Deleted comments and added text to ReadMe file

--- Assets/script.js
+++ Assets/script.js
@@ -1,16 +1,8 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html. 
+
 $(function () {
 
   runDateLogic();
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. 
-     // HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
+    
     
     var saveContent = document.getElementById("text")
     
@@ -28,11 +20,7 @@ $(function () {
  
     
    
-    // // TODO: Add code to apply the past, present, or future class to each time
-    // // block by comparing the id to the current hour. HINTS: How can the id
-    // // attribute of each time-block be used to conditionally add or remove the
-    // // past, present, and future classes? How can Day.js be used to get the
-    // // current hour in 24-hour time?
+    
     let current_date = new Date();
     let current_hour = current_date.getHours();
     console.log(current_hour)
@@ -86,14 +74,14 @@ $(function () {
         var element = document.querySelector(hourArr[i].id);
         element.classList.add("future")
       }
-      else if (hourArr[i] === current_hour){
+      else if (hourArr[i].hour.getHours() === current_hour){
         var element = document.querySelector(hourArr[i].id);
         element.classList.add("present")
       }
     }
     
 
-    // // TODO: Add code to display the current date in the header of the page.
+  
   });
 
   const setHour = function(hour) {
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # daily-scheduler
+
+## Project Description 
+This project is a daily scheduler which displays the current, future, and past hours through color (past= gray, present = red, and future = green).This scheduler receives text and stores it in local storage with a label according to the given hour. The scheduler also displays the current date on the top of the page. 
+
+## Source Code 
+Starting code for HTML and CSS was provided by the Northwestern Coding Camp and is found under the main and in the assets folder. 
+
+## Where you can find the project 
+GitHub repo: 
+Deployed URL: 
